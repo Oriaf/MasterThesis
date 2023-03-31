7bd8b3189835af265969b7b0dbb8ade1a0c9a680
Alexander Danielsson <arox99@hotmail.se>
2023-03-31 16:24:51 +0200

    The sonification now sonifies the catheter's angle

--- Assets/Scripts/Sonification.cs
+++ Assets/Scripts/Sonification.cs
@@ -12,9 +12,19 @@ public class Sonification : MonoBehaviour
 	public float x = 0;
 	public float y = 0;
 	
+	[Header("Sonification Settings")]
 	public SonificationType sonification = SonificationType.Simple;
+	
+	[Header("Sonification Data")]
+	public Transform catheter;
+	public Collider patientSkull;
+	public Vector3 entryPoint;
+	
+	// Data Variables
+	private Vector3 targetNormal;
+	private Vector3 angles;
 
-	// Partial constants
+	// Sonification Instruments
 	private ShepardTone shepard;
 	private SimpleTone simple;
 	private SpatialTone spatial;
@@ -26,6 +36,20 @@ public class Sonification : MonoBehaviour
 		simple = new SimpleTone(130, 0.25f, 48000, 1.0f, GetComponent<AudioSource>());
 		spatial = new SpatialTone(0.5f, 48000);
 		
+		angles = Vector3.zero;
+		
+		// Calculate the target alignment
+		RaycastHit hit;
+		if(patientSkull.Raycast(new Ray(entryPoint - new Vector3(1,1,1), new Vector3(1,1,1)), out hit, 10.0f)){
+			if(!hit.point.Equals(entryPoint)) Debug.LogWarning("Something went wrong with the raycast!");
+			
+			targetNormal = hit.normal;
+		}
+		else{
+			Debug.LogError("Could not find the surface normal of the patient's skull!");
+			
+			targetNormal = Vector3.zero;
+		}
     }
 	
 	private Instrument getInstrument(){
@@ -52,13 +76,39 @@ public class Sonification : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	
+		Debug.Log(catheter.up + ", " + targetNormal);
+		
+		/*
+			1. Project both vectors onto the x-z plane
+			2. Calculating the required yaw angle
+			3. Rotate catheter vector by the required yaw angle
+			4. Calculate the required pitch angle
+			
+			Pitch and yaw will always be the shortest angle to the target angle
+			Pitch increases as the catheter's tip moves towards the world's positive y-axis
+			Yaw increases as the catheter's tip moves in the x-z plane towards the negative z-axis clockwise
+		*/
+		
+		// 1. Project both vectors onto the x-z plane
+		Vector3 catProj = Vector3.ProjectOnPlane(catheter.up, Vector3.up);
+		Vector3 targetProj = Vector3.ProjectOnPlane(targetNormal, Vector3.up);
+		// 2. Calculating the required yaw angle
+		float yaw = Vector3.SignedAngle(catProj, targetProj, Vector3.up);
+		angles.x = yaw;
+		
+		// 3. Rotate catheter vector by the required yaw angle
+		Quaternion rot = new Quaternion();
+		rot.eulerAngles = new Vector3(0, yaw, 0);
+		Vector3 rotCat = rot * catheter.up;
+		// 4. Calculate the required pitch angle
+		float pitch = Vector3.SignedAngle(rotCat, targetNormal, Vector3.one);
+		angles.y = pitch;
+		
+		Debug.Log(angles / 180f);
     }
 	
 	void OnAudioFilterRead(float[] data, int channels){
-		Vector3 pos = Vector3.zero;
-		pos.x = x;
-		pos.y = y;
+		Vector3 pos = angles / 180f;
 		
 		Instrument instrument = getInstrument();
 		instrument.sampleInstrument(data, channels, pos);
